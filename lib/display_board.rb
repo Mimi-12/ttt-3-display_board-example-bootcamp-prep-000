d683ef407fb6350a244166c614044e05f081c22a
selam lemma <oneiconforlife26@gmail.com>
2020-09-20 00:47:35 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,16 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
+<<<<<<< HEAD
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
+=======
+  puts "  |    |   "
+  puts "-----------"
+  puts "  |    |   "
+  puts "-----------"
+  puts "  |    |   "
+>>>>>>> 2af8d1fde2899846d90431c1b9b46148b67b1abc
 end
